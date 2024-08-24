b20f4e4d89fe12fb4e6e2a5480543476dc1054bd
Amulya Shetty <79209035+amulyaashetty@users.noreply.github.com>
2024-08-24 11:07:12 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Chat from '../src/components/Chat/Chat';
+import Tabs from './Tabs';
+import Footer from './Footer';
+import "../src/App.css"
+function App() {
+  return (
+    
+    <div className="app">
+      <div className='appWrapper'>
+        
+<div className='innerWrapper'>
+
+      <header className="header">
+        <img src= "./image.png" alt="Flexera Logo" className="logo" />
+        <img src="./bot.png" alt="Bot Logo" className="botlogo" />
+      </header>
+      <Tabs />
+      <Chat />
+      <Footer />
+        
+</div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
